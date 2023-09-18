aeebf126e42ebfae5d7d010980bd50545814d273
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-18 16:36:56 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,16 @@
+1. select name from students
+
+2. select name from students where Age>30
+
+3. select name from students WHERE Gender = 'F' and age = 30
+
+4. select name ,Points from students where name ="Alex"
+
+5. INSERT INTO students ( Name , Age , Gender , Points)
+VALUES ('Rayan' , 23 , 'F' , 300)
+
+
+6. UPDATE students set Points =400 WHERE name='Basma'
+
+
+7. UPDATE students set Points =100 WHERE name='Alex'
