1b52ed2995d5a84cf21de07515e3a76a48e09611
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-18 17:24:27 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -6,11 +6,44 @@
 
 4. select name ,Points from students where name ="Alex"
 
-5. INSERT INTO students ( Name , Age , Gender , Points)
-VALUES ('Rayan' , 23 , 'F' , 300)
+5. 
+#   IANSERT NEW VALUES FOR A TABLE 
+
+        INSERT INTO students ( Name , Age , Gender , Points)
+        VALUES ('Rayan' , 23 , 'F' , 300)
 
 
 6. UPDATE students set Points =400 WHERE name='Basma'
 
 
-7. UPDATE students set Points =100 WHERE name='Alex'
+7.  
+    # change value to another one 
+        UPDATE students set Points =100 WHERE name='Alex'
+
+10.   CREATE TABLE geaduates
+        AS SELECT
+        *
+        FROM students
+
+ 11. 
+#   create a new table from an existing table (copy)
+        CREATE TABLE graduates
+        AS SELECT
+        *
+        FROM students
+        where name='Layal'   
+
+
+12.
+#    -- add new column with date type 
+        ALTER TABLE graduates
+        ADD graduation date;
+        # -- set value for existing colun (date value)
+        UPDATE graduates
+        SET graduation = '08/09/2018'
+        WHERE name = 'Layal';    
+
+13. 
+#    delete values from table whith condition
+        DELETE from students WHERE name='Layal'
+        
